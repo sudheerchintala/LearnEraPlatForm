dda717afb9b7d2092fecb253c66996c0950f4a8a
Jesse Zoldak <zoldak@edx.org>
2014-08-28 14:52:47 -0400

    Provide fault-tolerance for adding another bok-choy shard to jenkins

--- scripts/all-tests.sh
+++ scripts/all-tests.sh
@@ -93,7 +93,11 @@ fi
 # Activate the Python virtualenv
 source $HOME/edx-venv/bin/activate
 
-# If environment variable 'SHARD' is not set, default to 'all'
+# If the environment variable 'SHARD' is not set, default to 'all'.
+# This could happen if you are trying to use this script from
+# jenkins and do not define 'SHARD' in your multi-config project.
+# Note that you will still need to pass a value for 'TEST_SUITE'
+# or else no tests will be executed.
 SHARD=${SHARD:="all"}
 
 case "$TEST_SUITE" in
@@ -120,27 +124,69 @@ END
         ;;
 
     "lms-acceptance")
-        paver test_acceptance -s lms --extra_args="-v 3 --tag shard_${SHARD}"
+        case "$SHARD" in
+
+            "all")
+                paver test_acceptance -s lms --extra_args="-v 3"
+                ;;
+
+            "*")
+                paver test_acceptance -s lms --extra_args="-v 3 --tag shard_${SHARD}"
+                ;;
+        esac
         ;;
 
     "cms-acceptance")
-        paver test_acceptance -s cms --extra_args="-v 3 --tag shard_${SHARD}"
+        case "$SHARD" in
+
+            "all")
+                paver test_acceptance -s cms --extra_args="-v 3"
+                ;;
+
+            "*")
+                paver test_acceptance -s cms --extra_args="-v 3 --tag shard_${SHARD}"
+                ;;
+        esac
         ;;
 
     "bok-choy")
         case "$SHARD" in
+
+            "all")
+                paver test_bokchoy
+                paver bokchoy_coverage
+                ;;
+
             "1")
                 paver test_bokchoy --extra_args="-a shard_1"
+                paver bokchoy_coverage
                 ;;
 
             "2")
                 paver test_bokchoy --extra_args="-a '!shard_1'"
-               ;;
-            "all")
-                paver test_bokchoy
-               ;;
+                paver bokchoy_coverage
+                ;;
+
+            # Default case because if we later define another bok-choy shard on Jenkins
+            # (e.g. Shard 3) in the multi-config project and expand this file
+            # with an additional case condition, old branches without that commit
+            # would not execute any tests on the worker assigned to that shard
+            # and thus their build would fail.
+            # This way they will just report 1 test executed and passed.
+            "*")
+                # Need to create an empty test result so the post-build
+                # action doesn't fail the build.
+                # May be unnecessary if we changed the "Skip if there are no test files"
+                # option to True in the jenkins job definitions.
+                mkdir -p reports
+                cat > reports/bok_choy/xunit.xml <<END
+<?xml version="1.0" encoding="UTF-8"?>
+<testsuite name="nosetests" tests="1" errors="0" failures="0" skip="0">
+<testcase classname="acceptance.tests" name="shard_placeholder" time="0.001"></testcase>
+</testsuite>
+END
+                ;;
         esac
-        paver bokchoy_coverage
         ;;
 
 esac
